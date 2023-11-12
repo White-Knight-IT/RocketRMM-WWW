4abe532cf4ac5dc1afbbad8d7414bdede1644b62
Ian Harris - White Knight IT <ian@whiteknightit.com.au>
2023-11-12 13:17:49 +1100

    latest

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ by the client. */
 
 const config = {
   auth: {
-    clientId: '06df2af7-2672-4509-b185-5b5a13108f82',
+    clientId: 'e73ed9aa-5926-4f24-992c-c9448d7e713a',
     authority: 'https://login.microsoftonline.com/organizations/',
     redirectUri: 'https://localhost',
     postLogoutRedirectUri: 'https://localhost/signedout'
@@ -13,9 +13,9 @@ const config = {
     storeAuthStateInCookie: false
   },
   api: {
-    scopes: ['https://whiteknightit.com.au/0f47fcb9-5070-40ac-813a-ee655b59cb0f/rocketrmm-api.access'],
+    scopes: ['https://whiteknightit.com.au/de8e827a-7799-48a7-a23f-23c6de81cf75/rocketrmm-api.access'],
     swagger: true,
-    deviceTag: '327796'
+    deviceTag: '0bb2ad'
   },
   ui: {
     frontEndUrl: 'https://localhost',
